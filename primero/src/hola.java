f2380426c41bca1ec9d901dc108a7482906b79c0
ud93 <Cristian@Cristian-PC>
2013-02-28 16:58:30 +0000

    quitando mensaje

--- primero/src/hola.java
+++ primero/src/hola.java
@@ -5,9 +5,7 @@ public class hola {
 		System.out.println("gracias por su visita");
 	}
 	
-	public void Despedida2() {
-		System.out.println("Se tuvo que ir");
-	}
+	
 	public static void main(String[]args){
 		System.out.println("Buenos dias");
 	}
