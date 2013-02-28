31e0e7e7046aa88782bfdaec17d50aa5a2113703
Cristian <Cristian@Cristian-PC>
2013-02-28 16:46:12 +0000

    colaborador windows cristian

--- primero/src/hola.java
+++ primero/src/hola.java
@@ -1,6 +1,11 @@
 
 public class hola {
-
+	
+	public void Despedida() {
+		System.out.println("gracias por su visita");
+		
+	}
+	
 	public static void main(String[]args){
 		System.out.println("Buenos dias");
 	}
