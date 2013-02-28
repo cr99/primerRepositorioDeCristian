4fca4c76efe18f0e55e04e96b897d8ddc2796878
Cristian <Cristian@Cristian-PC>
2013-02-28 16:52:33 +0000

    windows 2 cristian

--- primero/src/hola.java
+++ primero/src/hola.java
@@ -3,9 +3,11 @@ public class hola {
 	
 	public void Despedida() {
 		System.out.println("gracias por su visita");
-		
 	}
 	
+	public void Despedida2() {
+		System.out.println("Se tuvo que ir");
+	}
 	public static void main(String[]args){
 		System.out.println("Buenos dias");
 	}
