d5f8db5ad22389ff9162c1d3d8aa155c836df1e0
ud93 <cris_lp93@hotmail.com>
2013-03-01 13:17:36 +0000

    cambios cristian 4

--- primero/src/hola.java
+++ primero/src/hola.java
@@ -9,10 +9,15 @@ public class hola {
 		System.out.println("vete a la mierda2");
 	}
 	
+	public int suma(int a, int b) {
+		int res = a + b;
+		return res;
+		
+	}
 	public static void main(String[]args){
 		System.out.println("Buenos dias");
 	}
 	
-	
+
 	
 }
